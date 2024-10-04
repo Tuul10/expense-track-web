1b951386154bb25d359ac8a5fa7e958db133c4f6
24LP8078 24LP8078 <24LP8078@G305-CC0038.local>
2024-10-04 14:39:52 +0800

    aa

---
+++ controller/records/deleteRecord.js
@@ -0,0 +1,12 @@
+import { sql } from "../../database";
+
+export const deleteRecord = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const deleteUser = await sql`DELETE FROM records
+                                 WHERE userid = ${id}`;
+    response.status(200).json({ users: deleteUser });
+  } catch (error) {
+    response.status(400).json({ error: error });
+  }
+};
--- router/recordsRouter.js
+++ router/recordsRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getRecord } from "../controller/records/getRecord";
 import { postRecord } from "../controller/records/postRecord";
+import { deleteRecord } from "../controller/records/deleteRecord";
 
 export const recordsRouter = express.Router();
 
 recordsRouter.get("/", getRecord);
 recordsRouter.post("/", postRecord);
+recordsRouter.delete("/delete", deleteRecord);
