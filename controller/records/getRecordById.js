693db7b1624b81584946e224484d53bfc02f3aff
24LP8078 24LP8078 <24LP8078@G305-CC0038.local>
2024-10-09 13:43:23 +0800

    mm

--- controller/records/getRecordById.js
+++ controller/records/getRecordById.js
@@ -1,10 +1,11 @@
 import { sql } from "../../database/index";
 
-export const getRecord = async (request, response) => {
+export const getRecordById = async (request, response) => {
   try {
+    const { id } = request.params;
     const records = await sql`SELECT * FROM records
                               INNER JOIN categories ON records.categoryid = categories.categoryid
-                             `;
+                              WHERE records.categoryid = ${id}`;
 
     response.status(200).json({ records: records });
   } catch (error) {
--- router/recordsRouter.js
+++ router/recordsRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getRecord } from "../controller/records/getRecord";
+import { getRecordById } from "../controller/records/getRecordById";
 import { postRecord } from "../controller/records/postRecord";
 import { deleteRecord } from "../controller/records/deleteRecord";
+import { getRecord } from "../controller/records/getRecord";
 
 export const recordsRouter = express.Router();
 
+recordsRouter.get("/:id", getRecordById);
 recordsRouter.get("/", getRecord);
-recordsRouter.post("/", postRecord);
+recordsRouter.post("/:id", postRecord);
 recordsRouter.delete("/:id", deleteRecord);
